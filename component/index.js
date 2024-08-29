6e49107a899d9e450439dfc0303cec528265c2d1
vikram singh <146583704+hyperlink255@users.noreply.github.com>
2024-08-29 21:47:37 +0530

    added index.html

---
+++ component/index.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("header.html")
+    .then((res) => {
+        if(!res.ok){
+            throw new Error ("Network response was not ok")
+        }
+        return res.text()
+    }).then((content) => {
+        document.querySelector(".header-container").innerHTML = content
+        window.addEventListener('scroll', () => {
+        document.querySelector('.navbar').classList.toggle
+        ('window-scroll', window.scrollY > 5)
+        })  
+        const navbarToggle = document.querySelector('.navbar-toggle')
+        const headerRight = document.querySelector('.header-right')
+         navbarToggle.addEventListener('click', () => {
+            headerRight.classList.toggle('active')
+         })
+         window.addEventListener('scroll',() => {
+            document.querySelectorAll('.zoom').forEach(item => {
+                item.classList.toggle('animation', window.scrollY > 10)
+            })    
+         })
+          
+        const center = document.querySelectorAll('.item')
+        center.forEach(item => {
+        item.addEventListener('click',() => {
+            console.log('asdn')
+            center.forEach(item => {
+                item.classList.remove('active')
+            })
+            item.classList.add('active')
+        })
+        })
+        
+        
+    }).catch((error) => {
+        document.querySelector('.header-container').innerHTML = "Error loading header"
+    })
+    
+    fetch("footer.html")
+    .then((res) => {
+        if(!res.ok){
+            throw new Error("Network response was not ok");
+        }
+        return res.text()
+    }).then((res) => {
+        document.querySelector(".footer-data").innerHTML = res
+    }).catch((err) => {
+        console.log('footer-error', err)
+        document.querySelector(".footer-data").innerHTML = "Error loading footer"
+    })
+    
+    
+    function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+    }
+    function fadeOut(){
+        setInterval(loader,2000)
+    }
+    window.onload = fadeOut
+    
+    })
+    
---
+++ index.js
@@ -0,0 +1,40 @@
+
+document.addEventListener("DOMContentLoaded" , () => {
+window.addEventListener('scroll', () => {
+document.querySelector('.navbar').classList.toggle
+('window-scroll', window.scrollY > 5)
+})  
+const navbarToggle = document.querySelector('.navbar-toggle')
+const headerRight = document.querySelector('.header-right')
+ navbarToggle.addEventListener('click', () => {
+    headerRight.classList.toggle('active')
+    let nav = navbarToggle.querySelector("i")
+    nav.classList.toggle('fa-remove')
+
+ })
+ window.addEventListener('scroll',() => {
+    document.querySelectorAll('.zoom').forEach(item => {
+        item.classList.toggle('animation', window.scrollY > 10)
+    })    
+ })
+  
+const center = document.querySelectorAll('.item')
+center.forEach(item => {
+item.addEventListener('click',() => {
+    console.log('asdn')
+    center.forEach(item => {
+        item.classList.remove('active')
+    })
+    item.classList.add('active')
+})
+})
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+    }
+    function fadeOut(){
+        setInterval(loader,2000)
+    }
+    window.onload = fadeOut
+    
+})
